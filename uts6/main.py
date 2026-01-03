002eef65564125d4d4e4cada131eb25447050e2a
ni8234716-byte <ni8234716@gmail.com>
2026-01-03 14:07:08 +0800

    Update main.py

--- uts6/main.py
+++ uts6/main.py
@@ -6,3 +6,17 @@ nilai = int(input("Masukkan nilai: "))
 hasil = run(nilai)
 print("Kategori Nilai:", hasil)
 
+print("\n====soal nomor 2====")
+from logika import konversi_nilai
+
+print("=== penentuan nilai berdasarkan huruf ===")
+
+huruf = input("Masukkan huruf nilai: ")
+
+nilai = konversi_nilai(huruf)
+
+if nilai is not None:
+    print("Nilai Angka =", nilai)
+else:
+    print("Huruf nilai tidak valid ")
+
